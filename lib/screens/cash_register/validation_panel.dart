e17f1683e6f756ac5d09cc591b7098d1ba409f8a
LaurieBe <lb@gmail.com>
2023-01-11 16:32:09 +0100

    UI cash page

--- lib/screens/cash_register/validation_panel.dart
+++ lib/screens/cash_register/validation_panel.dart
@@ -7,9 +7,7 @@ import 'package:provider/provider.dart';
 
 class ValidationPanel extends StatefulWidget {
   const ValidationPanel(
-      {super.key,
-      required this.orderDao,
-      required this.formKey});
+      {super.key, required this.orderDao, required this.formKey});
 
   final GlobalKey<FormState> formKey;
   final OrderDao orderDao;
@@ -36,6 +34,11 @@ class _ValidationPanelState extends State<ValidationPanel> {
   Widget build(BuildContext context) {
     log('build ValidationPanel');
 
+    var styleHeaders = Theme.of(context)
+        .primaryTextTheme
+        .titleLarge
+        ?.apply(color: Theme.of(context).colorScheme.primary);
+
     return Consumer<AppModel>(builder: (context, model, child) {
       double subtotal = model.cart
           .map((e) =>
@@ -50,87 +53,98 @@ class _ValidationPanelState extends State<ValidationPanel> {
 
       double total = subtotal + cardFee;
 
-      return Column(
-        children: [
-          Row(children: [
-            Expanded(
-                child: Autocomplete<Member>(
-              displayStringForOption: (Member m) => '${m.name} : ${m.email}',
-              optionsBuilder: (TextEditingValue textEditingValue) async {
-                if (textEditingValue.text == '') {
-                  return const Iterable<Member>.empty();
-                }
-                return model.members.where((Member m) {
-                  return m
-                      .toString()
-                      .contains(textEditingValue.text.toLowerCase());
-                });
-              },
-              onSelected: (m) {
-                setState(() {
-                  selectedMember = m;
-                });
-              },
-            )),
-          ]),
-          Row(children: [
-            const Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text('Mode de paiment: '))),
-            Expanded(
-                child: DropdownButton<String>(
-              value: paymentmethodSelected,
-              elevation: 16,
-              onChanged: (String? value) {
-                // This is called when the user selects an item.
-                setState(() {
-                  paymentmethodSelected = value!;
-                });
-              },
-              items: paymentMethodList
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ))
-          ]),
-          //Todo: add a text box to enter cheque number
-          //if(paymentmethodSelected == 'Cheque') {widgets...}
-          Row(children: [
-            const Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft, child: Text('Sous total: '))),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text('${subtotal.toStringAsFixed(2)}€')))
-          ]),
-          Row(children: [
-            const Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft, child: Text('Total: '))),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text('${total.toStringAsFixed(2)}€')))
-          ]),
-          Center(
-              child: ElevatedButton(
-            onPressed: () {
-              if (_validateAll()) {
-                log('Send form !!!');
-                _sendForm(model);
-              } else {
-                log('Form invalid');
-              }
-            },
-            child: const Text('Valider'),
-          ))
-        ],
-      );
+      return Container(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Row(children: [
+                Expanded(
+                    child: Text(
+                  'Membre',
+                  style: styleHeaders,
+                )),
+              ]),
+              Row(children: [
+                Expanded(
+                    child: Autocomplete<Member>(
+                  displayStringForOption: (Member m) =>
+                      '${m.name} : ${m.email}',
+                  optionsBuilder: (TextEditingValue textEditingValue) async {
+                    if (textEditingValue.text == '') {
+                      return const Iterable<Member>.empty();
+                    }
+                    return model.members.where((Member m) {
+                      return m
+                          .toString()
+                          .contains(textEditingValue.text.toLowerCase());
+                    });
+                  },
+                  onSelected: (m) {
+                    setState(() {
+                      selectedMember = m;
+                    });
+                  },
+                )),
+              ]),
+              Row(children: [
+                const Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('Mode de paiment: '))),
+                Expanded(
+                    child: DropdownButton<String>(
+                  value: paymentmethodSelected,
+                  elevation: 16,
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      paymentmethodSelected = value!;
+                    });
+                  },
+                  items: paymentMethodList
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ))
+              ]),
+              //Todo: add a text box to enter cheque number
+              //if(paymentmethodSelected == 'Cheque') {widgets...}
+              Row(children: [
+                const Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('Sous total: '))),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('${subtotal.toStringAsFixed(2)}€')))
+              ]),
+              Row(children: [
+                const Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft, child: Text('Total: '))),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('${total.toStringAsFixed(2)}€')))
+              ]),
+              Center(
+                  child: ElevatedButton(
+                onPressed: () {
+                  if (_validateAll()) {
+                    log('Send form !!!');
+                    _sendForm(model);
+                  } else {
+                    log('Form invalid');
+                  }
+                },
+                child: const Text('Valider'),
+              ))
+            ],
+          ));
     });
   }
 
